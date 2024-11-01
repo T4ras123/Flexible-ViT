b85c9788314722f251ac37866b6a3e134b4aab95
T4ras123 <vovatara123@gmail.com>
2024-11-01 12:48:09 +0400

    feat: refactor VisionTransformer implementation and add image preprocessing components

--- ViT/train.py
+++ ViT/train.py
@@ -1,133 +1,129 @@
-import torch 
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.optim import Adam
-
-H, W, C = 224, 224, 3   # height x width x channels (3 - red, green, blue)
-batch_size = 4096
-P = 16                  # resolution of a patch
-N = H*W//P**2            # Number of patches 
-D = 768                 # Constant latent layer
-lr = 2e-4
-max_iters = 1000000
-n_layers = 12
-num_heads = 12
-head_size = D//num_heads
-num_classes = 1000
+import torch
+from torch import nn
+from einops.layers.torch import Rearrange
+from torch import Tensor
+from torchvision.datasets import OxfordIIITPet
+import matplotlib.pyplot as plt
+from random import random
+from torchvision.transforms import Resize, ToTensor
+from torchvision.transforms.functional import to_pil_image
+from einops import repeat
 
 
+to_tensor = [Resize((144,144)), ToTensor()]
+
+
+class Compose(object):
+  def __init__(self, transforms):
+    self.transforms = transforms
     
+  def __call__(self, image):
+    for t in self.transforms:
+      image = t(image)
+      
+    return image
+  
+def show_img(images, num_samples=20, columns=4):
+  plt.figure(figsize=(15,15))
+  idx = int(len(dataset)/num_samples)
+  for i, img in enumerate(images):
+    if i% idx == 0:
+      plt.subplot(int(num_samples/columns)+1, columns, int(i/idx)+1)
+      plt.imshow(to_pil_image(img[0]))
+  
+  
+dataset = OxfordIIITPet(root=".", download=True, transform=Compose(to_tensor))
 
-class VisionTransformer(nn.Module):
-    
-    def __init__(self) -> None:
-        super().__init__()
-        self.patch_embedding = nn.Embedding(C*P**2, D)
-        self.positional_embedding = nn.Embedding(N+1, D)
-        self.classification = nn.Parameter(torch.randn(1, 1, D))
-        self.blocks = nn.Sequential(*[Block() for _ in range(n_layers)])
-        self.l_head = nn.Linear(D, num_classes)
 
-        
-    def forward(self, ix, targets = None): # ix.shape (batch_size x N x (C*P**2)) 
-        patch_emb = self.patch_embedding(ix)
-        pos_emb = self.positional_embedding(torch.arange(N+1))
-        cls_token = self.classification.expand(batch_size, -1, -1)  # (batch_size, 1, D)
-        ix = torch.cat((cls_token, pos_emb), dim=1)
-        ix = patch_emb + pos_emb
-        ix = self.blocks(ix)
-        logits = self.l_head(ix)
-        
-        if targets is None:
-            loss = None
-        else:
-            B, T, C = logits.shape
-            logits = logits.view(B * T, C)
-            targets = targets.view(B * T)
-            loss = F.cross_entropy(logits, targets)
-            
-        return logits, loss
+class PatchEmbedding(nn.Module):
+  def __init__(self, in_channels=3, patch_size = 8, emb_size = 128):
+    super().__init__()
+    self.patch_size = patch_size
+    self.projection = nn.Sequential(
+      Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_size, p2=patch_size),
+      nn.Linear(patch_size*patch_size*in_channels, emb_size)
+    )
     
-            
+  def forward(self, x: Tensor) -> Tensor:
+    x = self.projection(x)
+    return x 
+
+
 class Block(nn.Module):
     
-    def __init__(self) -> None:
+    def __init__(self, emb_dim=32) -> None:
         super().__init__()
-        self.sa = MultiHeadAttention()
+        self.sa = Attention()
         self.ffwd = FeedForward()
-        self.ln1 = nn.LayerNorm(D)
-        self.ln2 = nn.LayerNorm(D)
+        self.ln1 = nn.LayerNorm(emb_dim)
+        self.ln2 = nn.LayerNorm(emb_dim)
 
 
     def forward(self, ix):
         ix = ix + self.ffwd(self.ln1(ix))
         ix = ix + self.sa(self.ln2(ix))
         return ix
-        
+
+
+class Attention(nn.Module):
+  def __init__(self, dim, n_heads, dropout) -> None:
+    super().__init__()
+    self.n_heads = n_heads
+    self.att = torch.nn.MultiheadAttention(
+      embed_dim=dim,
+      num_heads=n_heads,
+      dropout=dropout
+    )
+    self.q = nn.Linear(dim, dim)
+    self.v = nn.Linear(dim, dim)
+    self.k = nn.Linear(dim, dim)
     
-class Head(nn.Module):
-    
-    def __init__(self) -> None:
-        super().__init__()
-        self.key = nn.Linear(D, head_size, bias=False)
-        self.query = nn.Linear(D, head_size,  bias=False)
-        self.value = nn.Linear(D, head_size,  bias=False)
-        self.head_size = head_size
-        
-        
-    def forward(self, ix): 
-        k = self.key(ix)
-        q = self.query(ix)
-        
-        wei = q @ k.transpose(-2, -1) * self.head_size**-0.5
-        wei = F.softmax(wei, dim=-1)
-        v = self.value(ix)
+  def forward(self, x):
+    q = self.q(x)
+    v = self.v(x)
+    k = self.k(x)
+    attn_output, _ = self.att(q, k, v)
+    return attn_output
 
-        out = wei @ v
-        
-        return out
+class FeedForward(nn.Sequential):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
 
 
-class MultiHeadAttention(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.heads = num_heads
-        self.proj = nn.Linear(D, D)
-        
-        
-    def forward(self, ix):
-        out = torch.cat([ix for _ in range(self.heads)], dim=-1)
-        return out
+class ViT(nn.Module):
+  def __init__(self, ch=3, img_size=144, patch_size=4, emb_dim=32,
+                n_layers=6, out_dim=37, dropout=0.1, heads=2):
+    super(ViT, self).__init__()
 
-        
-class FeedForward(nn.Module):
-    
-    def __init__(self) -> None:
-        super().__init__()
-        self.lin1 = nn.Linear(D, 4*D)
-        self.tanh = nn.Tanh()
-        self.lin2 = nn.Linear(4*D, D)
+    self.channels = ch
+    self.height = img_size
+    self.width = img_size
+    self.patch_size = patch_size
+    self.n_layers = n_layers
+    self.blocks = nn.Sequential(*[Block() for _ in range(n_layers)])
+    self.l_head = nn.Linear(emb_dim, out_dim)
+
+    self.patch_embedding = PatchEmbedding(in_channels=ch, patch_size=patch_size, emb_size=emb_dim)
+    num_patches = (img_size // patch_size) ** 2
+    self.pos_embedding = nn.Parameter(
+        torch.randn(1, num_patches + 1, emb_dim))
+    self.cls_token = nn.Parameter(torch.rand(1, 1, emb_dim))
     
     
-    def forward(self, ix):
-        out = self.lin1(ix)
-        out = self.tanh(out)
-        out = ix + self.lin2(out)
-        return out
-    
+  def forward(self, img):
+    x = self.patch_embedding(img)
+    b, n, _ = x.shape
+    cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
+    x = torch.cat([cls_tokens, x], dim=1)
+    x += self.pos_embedding[:, :(n + 1)]
 
-if __name__=='__main__':
+    for i in range(self.n_layers):
+        x = self.layers[i](x)
 
-    model = VisionTransformer()
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-
-    xb, yb = 0,0 # for lack of actual data
-
-    for _ in range(max_iters):
-    
-        logits ,loss = model(xb, yb)
-        optimizer.zero_grad(set_to_none=True)
-        loss.backward()
-        optimizer.step()
-    
+    return self.head(x[:, 0, :])
